78857e6a8e2b837f3b953c3fc84e30576efee53a
Tarek Abdo <tarekk.abdo22@gmail.com>
2023-12-02 00:11:59 +0200

    ended

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    {/* <StarRating maxRating={9} /> */}
   </React.StrictMode>
 );
